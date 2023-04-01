6a1613ac263db14aadf3a2c1ae06b3e3d222f60f
Bryce Anderson <bryce.anderson22@gmail.com>
2023-04-01 10:21:29 -0600

    Starting with the main app

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,44 @@ module Main (main) where
 
 import Lib
 
+data Config = Config {
+   dictPath :: Maybe String,
+   required :: Char,
+   others :: String
+  } deriving (Show)
+
+getConfig :: IO Config
+getConfig = do
+  return Config { dictPath = Nothing, required = 'a', others = "abcdefg" }
+
 main :: IO ()
 main = do
-  dict <- makeDict Nothing
+  config <- getConfig
+  helloMessage
+  dict <- makeDict (dictPath config)
   putStrLn $ show dict
+
+helloMessage :: IO ()
+helloMessage = do
+  putStrLn "🐝"
+  putStrLn "Hello and welcome to Spelling Bee Solver"
+  putStrLn "🐝🐝"
+
+  putStrLn "🐝🐝🐝"
+  -- putStrLn "Required Letter:  ", string(required)
+  -- putStrLn "Other Letters:    ", others
+  -- putStrLn "Dictionary:       ", dictionaryName
+  -- putStrLn "Dictionary words: ", len(dictionary.Words())
+  putStrLn "Solving now"
+  putStrLn "🐝🐝🐝🐝"
+
+  -- solver := Solver{dictionary, puzzle}
+  -- start = time.Now()
+  -- solutions := solver.Solve()
+  -- elapsedSolve := time.Since(start)
+
+  putStrLn "🐝🐝🐝🐝🐝"
+  putStrLn "Solved!"
+  putStrLn ""
+  -- putStrLn "  Words:", len(solutions)
+
